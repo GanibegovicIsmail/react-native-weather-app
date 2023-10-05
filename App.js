105304f6e3b6dedd0fe7ef09c1427ae62947e45a
Ismail Ganibegovic <ismail.ganibegovic.20@size.ba>
2023-10-06 00:03:13 +0200

    Feat: Component Variable msg

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const message = 'Hello';
+
   return (
     <View style={{height: 100, width: 100, backgroundColor: "orange"}}>
-              <Text>Hello World</Text>
+              <Text>{message}</Text>
       </View>
   );
 }
