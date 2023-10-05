69989272fe1007a6406ff4b6462f3efc9cf8f0f5
Ismail Ganibegovic <ismail.ganibegovic.20@size.ba>
2023-10-05 23:50:02 +0200

    Feat: Hello World Orange

--- App.js
+++ App.js
@@ -3,10 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Promjena</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={{height: 100, width: 100, backgroundColor: "orange"}}>
+              <Text>Hello World</Text>
+      </View>
   );
 }
 
