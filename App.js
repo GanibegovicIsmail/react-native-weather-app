1e90c03c9830faed67190909709652c549d09454
Ismail Ganibegovic <ismail.ganibegovic.20@size.ba>
2023-10-06 11:01:16 +0200

    Fix: SafeAreaView

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import {View, Text} from  'react-native'
+import {View, Text,SafeAreaView,StyleSheet} from  'react-native'
+import GlobalStyles from './GlobalStyles';
 
 const App = () => {
   return (
-    <View>
+
+    <SafeAreaView style ={GlobalStyles.androidSafeArea}>
+    <View style = {styles.container}>
       <Text>Current Weather</Text>
     </View>
+    </SafeAreaView>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'pink',
+    flex:1
+  }
+})
 
 export default App
