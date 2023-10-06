5d7021cd434058a1c993900295a436e7a31b3edf
Ismail Ganibegovic <ismail.ganibegovic.20@size.ba>
2023-10-06 18:23:33 +0200

    Feat: Basic Layout

--- App.js
+++ App.js
@@ -7,15 +7,50 @@ const App = () => {
 
     <SafeAreaView style ={GlobalStyles.androidSafeArea}>
     <View style = {styles.container}>
-      <Text>Current Weather</Text>
+      <Text style={styles.temp}>6</Text>
+      <Text style = {styles.feels}>Feels like 5</Text>
+      <View style = {styles.highLowWrapper}>
+      <Text style = {styles.highLow}>High: 8</Text>
+      <Text style = {styles.highLow}>Low: 6</Text>
+      </View>
     </View>
+    <View style ={styles.bodyWrapper}>
+        <Text style={styles.description}>It's sunny</Text>
+        <Text style={styles.message}>It's perfect t-shirt weather</Text>
+      </View>
     </SafeAreaView>
   )
 }
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'pink',
-    flex:1
+    flex:1,
+    alignItems: 'center',
+  },
+  temp: {
+  color: 'black',
+  fontSize: 48
+  },
+  feels:{
+    fontSize:30,
+    color: 'black'
+  },
+  highLow:{
+    color: 'black',
+    fontSize: 20
+  },
+  highLowWrapper:{
+    flexDirection: 'row',
+  },
+  bodyWrapper:{
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start'
+  },
+  description:{
+    fontSize:48,
+  },
+  message:{
+    fontSize:30
   }
 })
 
