b0d59d57de6a8c390d017e5888c6ee4bcfe77a24
Ismail Ganibegovic <ismail.ganibegovic.20@size.ba>
2023-10-07 23:08:23 +0200

    feat: tab navigation

--- App.js
+++ App.js
@@ -3,11 +3,18 @@ import {View, StyleSheet} from 'react-native'
 import CurrentWeather from "./src/components/screens/CurrentWeather";
 import UpcomingWeather from "./src/components/screens/UpcomingWeather";
 import City from "./src/components/screens/City";
+import { NavigationContainer } from "@react-navigation/native";
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+
+const Tab = createBottomTabNavigator()
+
 const App = () =>{
   return (
+    <NavigationContainer>
 <View style={styles.container}>
   <CurrentWeather></CurrentWeather>
 </View>
+</NavigationContainer>
   )
 }
 const styles = StyleSheet.create({
