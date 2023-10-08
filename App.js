4b31b7742af9ed7e252e5f2b1d4b79cae35bce37
Ismail Ganibegovic <ismail.ganibegovic.20@size.ba>
2023-10-08 15:59:59 +0200

    fix: location request fix

--- App.js
+++ App.js
@@ -1,28 +1,66 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { ActivityIndicator, StyleSheet } from "react-native";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 import Tabs from "./src/components/Tabs";
+import * as Location from 'expo-location'
 
-const App = () =>{
-  const [loading, setLoading] = useState(true)
-  if (loading){
-    return(
-      <View style ={styles.container}>
-        <ActivityIndicator size={'large'} color={'blue'}/>
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const requestLocationPermission = async () => {
+      try {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setError('Permission to access location was denied');
+          setLoading(false);
+          return;
+        }
+
+        let locationData = await Location.getCurrentPositionAsync({});
+        setLocation(locationData);
+        setLoading(false);
+      } catch (error) {
+        setError('Error fetching location: ' + error.message);
+        setLoading(false);
+      }
+    };
+
+    requestLocationPermission();
+  }, []); // Empty dependency array means this effect runs once after the initial render
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'blue'} />
       </View>
-    )
+    );
   }
+
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Text>{error}</Text>
+      </View>
+    );
+  }
+
+  // Rest of your component code
+
   return (
     <NavigationContainer>
-      <Tabs/>
+      <Tabs />
     </NavigationContainer>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     justifyContent: 'center',
-    flex:1
+    flex: 1
   }
-})
+});
+
 export default App;
